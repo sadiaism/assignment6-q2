5c2b092bed12a8aad6995ae0ece934710e661efa
sadiaism <ismailmoiz80@gmail.com>
2025-02-13 10:59:30 +0500

    the ass

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,14 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-
-
-
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {}, // Turbo ko mention hi mat karein
+  /* config options here */
 };
 
 export default nextConfig;
+
+
+
+
+
+
+
